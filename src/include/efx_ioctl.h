4cc16def99c4f74172bde0feb9d301f54cb967f9
Andrew Bower <andrew.bower@amd.com>
2023-02-16 13:33:51 +0000

    SWPTP-1306: remove unnecessary efx definitions

--- src/include/efx_ioctl.h
+++ src/include/efx_ioctl.h
@@ -65,247 +65,7 @@
 
 #include "enum.h"
 
-/* Testing/QA: perform full reset while running *****************************/
-#define EFX_RESET 0xef01
-
-/* Parameters for EFX_RESET */
-struct efx_reset_ioctl {
-	enum reset_type method;
-};
-
-/* Testing/QA: re-acknowledge specified event queue *************************/
-#define EFX_EVQ_ACK 0xef02
-
-/* Parameters for EFX_EVQ_ACK */
-struct efx_evq_ack_ioctl {
-	/** Channel number */
-	__u16 channel;
-};
-
-/* Testing/QA: set loopback mode ********************************************/
-#define EFX_SET_LOOPBACK 0xef03
-
-/* Parameters for EFX_SET_LOOPBACK */
-struct efx_set_loopback_ioctl {
-	/** Loopback mode */
-	enum efx_loopback_mode mode;
-};
-
-#define LOOPBACK_NEAR	(LOOPBACK_MAX + 1)	/* loopback nearest to bus */
-#define LOOPBACK_FAR	(LOOPBACK_MAX + 2)	/* loopback furthest from bus */
-
-/* Testing/QA: debug MDIO access ********************************************/
-#define EFX_MDIO 0xef05
-
-/* Parameters for EFX_MDIO */
-struct efx_mdio_ioctl {
-	/** MDIO access */
-	__u16 read;
-	__u16 clause45;
-	__u32 value;
-	__u16 addr;
-	__u8 dev;
-	int prt;
-} __attribute__ ((packed));
-
-/* For setting the netif carrier on/off *************************************/
-#define EFX_SET_CARRIER 0xef09
-
-struct efx_set_carrier_ioctl {
-	__u8 on;
-};
-
-
-/* For resetting the XAUI link between Falcon and the PHY *******************/
-#define EFX_RESET_XAUI 0xef0a
-
-/* For setting the PHY power state ******************************************/
-#define EFX_SET_PHY_POWER 0xef0b
-
-struct efx_set_phy_power {
-	__u8 on;
-};
-
 #endif /* EFX_NOT_EXPORTED */
-
-/* For talking MCDI to siena ************************************************/
-
-/* Deprecated */
-#define EFX_MCDI_REQUEST 0xef0c
-/**
- * struct efx_mcdi_request - Parameters for %EFX_MCDI_REQUEST sub-command
- * @payload: On entry, the MCDI command parameters.  On return, if
- *	@rc == 0, this is the response.
- * @cmd: MCDI command type number.
- * @len: On entry, the length of command parameters, in bytes.  On
- *	return, if @rc == 0, this is the length of the response.
- * @rc: Linux error code for the request.  This may be based on an
- *	error code reported by the MC or a communication failure
- *	detected by the driver.
- *
- * If the driver detects invalid parameters, e.g. @len is out of
- * range, the ioctl() call will return -1, errno will be set
- * accordingly, and none of the fields will be valid.  All other
- * errors are reported by setting @rc.
- *
- * %EFX_MCDI_REQUEST does not support the larger command type numbers,
- * error codes and payload lengths of MCDIv2.
- */
-struct efx_mcdi_request {
-	__u32 payload[63];
-	__u8 cmd;
-	__u8 len;
-	__u8 rc;
-};
-
-#define EFX_MCDI_REQUEST2 0xef21
-/**
- * struct efx_mcdi_request2 - Parameters for %EFX_MCDI_REQUEST2 sub-command
- * @cmd: MCDI command type number.
- * @inlen: The length of command parameters, in bytes.
- * @outlen: On entry, the length available for the response, in bytes.
- *	On return, the length used for the response, in bytes.
- * @flags: Flags for the command or response.  The only flag defined
- *	at present is %EFX_MCDI_REQUEST_ERROR.  If this is set on return,
- *	the MC reported an error.
- * @host_errno: On return, if %EFX_MCDI_REQUEST_ERROR is included in @flags,
- *	the suggested Linux error code for the error.
- * @payload: On entry, the MCDI command parameters.  On return, the response.
- *
- * If the driver detects invalid parameters or a communication failure
- * with the MC, the ioctl() call will return -1, errno will be set
- * accordingly, and none of the fields will be valid.  If the MC reports
- * an error, the ioctl() call will return 0 but @flags will include the
- * %EFX_MCDI_REQUEST_ERROR flag.  The MC error code can then be found in
- * @payload (if @outlen was sufficiently large) and a suggested Linux
- * error code can be found in @host_errno.
- *
- * %EFX_MCDI_REQUEST2 fully supports both MCDIv1 and v2.
- */
-struct efx_mcdi_request2 {
-	__u16 cmd;
-	__u16 inlen;
-	__u16 outlen;
-	__u16 flags;
-	__u32 host_errno;
-	__u32 payload[0];
-};
-#define EFX_MCDI_REQUEST_ERROR	0x0001
-
-/* Reset selected components, like ETHTOOL_RESET ****************************/
-#define EFX_RESET_FLAGS 0xef0d
-struct efx_reset_flags {
-	__u32 flags;
-};
-#ifndef ETH_RESET_SHARED_SHIFT
-	enum ethtool_reset_flags {
-		/* These flags represent components dedicated to the interface
-		 * the command is addressed to.  Shift any flag left by
-		 * ETH_RESET_SHARED_SHIFT to reset a shared component of the
-		 * same type.
-		 */
-		ETH_RESET_MGMT		= 1 << 0,	/* Management processor */
-		ETH_RESET_IRQ		= 1 << 1,	/* Interrupt requester */
-		ETH_RESET_DMA		= 1 << 2,	/* DMA engine */
-		ETH_RESET_FILTER	= 1 << 3,	/* Filtering/flow direction */
-		ETH_RESET_OFFLOAD	= 1 << 4,	/* Protocol offload */
-		ETH_RESET_MAC		= 1 << 5,	/* Media access controller */
-		ETH_RESET_PHY		= 1 << 6,	/* Transceiver/PHY */
-		ETH_RESET_RAM		= 1 << 7,	/* RAM shared between
-							 * multiple components */
-
-		ETH_RESET_DEDICATED	= 0x0000ffff,	/* All components dedicated to
-							 * this interface */
-		ETH_RESET_ALL		= 0xffffffff,	/* All components used by this
-							 * interface, even if shared */
-	};
-	#define ETH_RESET_SHARED_SHIFT	16
-#endif
-#ifndef ETHTOOL_RESET
-	#define ETHTOOL_RESET           0x00000034
-#endif
-
-/* Get RX flow hashing capabilities, like ETHTOOL_GRX{RINGS,FH} *************/
-#define EFX_RXNFC 0xef0e
-#ifndef ETHTOOL_GRXFH
-	#define ETHTOOL_GRXFH		0x00000029
-#endif
-#ifndef ETHTOOL_GRXRINGS
-	#define ETHTOOL_GRXRINGS	0x0000002d
-#endif
-#ifndef ETHTOOL_GRXCLSRULE
-	struct ethtool_tcpip4_spec {
-		__be32	ip4src;
-		__be32	ip4dst;
-		__be16	psrc;
-		__be16	pdst;
-		__u8    tos;
-	};
-	#define RX_CLS_FLOW_DISC	0xffffffffffffffffULL
-	#define ETHTOOL_GRXCLSRLCNT	0x0000002e
-	#define ETHTOOL_GRXCLSRULE	0x0000002f
-	#define ETHTOOL_GRXCLSRLALL	0x00000030
-	#define ETHTOOL_SRXCLSRLDEL     0x00000031
-	#define ETHTOOL_SRXCLSRLINS	0x00000032
-#endif
-#ifndef EFX_HAVE_EFX_ETHTOOL_RXNFC
-	union efx_ethtool_flow_union {
-		struct ethtool_tcpip4_spec		tcp_ip4_spec;
-		struct ethtool_tcpip4_spec		udp_ip4_spec;
-		struct ethtool_tcpip4_spec		sctp_ip4_spec;
-		struct ethhdr				ether_spec;
-		/* unneeded members omitted... */
-		__u8					hdata[60];
-	};
-	struct efx_ethtool_flow_ext {
-		__be16	vlan_etype;
-		__be16	vlan_tci;
-		__be32	data[2];
-	};
-	struct efx_ethtool_rx_flow_spec {
-		__u32		flow_type;
-		union efx_ethtool_flow_union h_u;
-		struct efx_ethtool_flow_ext h_ext;
-		union efx_ethtool_flow_union m_u;
-		struct efx_ethtool_flow_ext m_ext;
-		__u64		ring_cookie;
-		__u32		location;
-	};
-	struct efx_ethtool_rxnfc {
-		__u32				cmd;
-		__u32				flow_type;
-		__u64				data;
-		struct efx_ethtool_rx_flow_spec	fs;
-		__u32				rule_cnt;
-		__u32				rule_locs[0];
-	};
-	#define EFX_HAVE_EFX_ETHTOOL_RXNFC yes
-#endif
-#ifndef RX_CLS_LOC_SPECIAL
-	#define RX_CLS_LOC_SPECIAL	0x80000000
-	#define RX_CLS_LOC_ANY		0xffffffff
-	#define RX_CLS_LOC_FIRST	0xfffffffe
-	#define RX_CLS_LOC_LAST		0xfffffffd
-#endif
-
-/* Get/set RX flow hash indirection table, like ETHTOOL_{G,S}RXFHINDIR} *****/
-#define EFX_RXFHINDIR 0xef10
-#ifndef ETHTOOL_GRXFHINDIR
-	struct ethtool_rxfh_indir {
-		__u32	cmd;
-		/* On entry, this is the array size of the user buffer.  On
-		 * return from ETHTOOL_GRXFHINDIR, this is the array size of
-		 * the hardware indirection table. */
-		__u32	size;
-		__u32	ring_index[0];	/* ring/queue index for each hash value */
-	};
-	#define ETHTOOL_GRXFHINDIR	0x00000038
-	#define ETHTOOL_SRXFHINDIR	0x00000039
-#endif
-struct efx_rxfh_indir {
-	struct ethtool_rxfh_indir head;
-	__u32 table[128];
-};
 
 /* PTP support for NIC time disciplining ************************************/
 
@@ -313,11 +73,6 @@ struct efx_timespec {
 	__s64	tv_sec;
 	__s32	tv_nsec;
 };
-
-/* Set/get hardware timestamp config, like SIOC{S,G}HWTSTAMP ****************/
-#define EFX_TS_INIT 0xef12
-
-#define EFX_TS_INIT_FLAGS_PTP_V2_ENHANCED 0x80000000
 
 #if !defined(__KERNEL__) || defined(__VMKLNX__)
 
@@ -425,33 +180,6 @@ struct efx_ts_set_sync_status {
 	#define ETHTOOL_GET_TS_INFO	0x00000041 /* Get time stamping and PHC info */
 #endif
 
-/* Get pluging module eeprom if not availble via ethtool ********************/
-#define EFX_MODULEEEPROM 0xef17
-#define EFX_GMODULEINFO 0xef18
-#ifndef ETHTOOL_GMODULEEEPROM
-	struct ethtool_modinfo {
-		__u32   cmd;
-		__u32   type;
-		__u32   eeprom_len;
-		__u32   reserved[8];
-	};
-
-	#define ETH_MODULE_SFF_8079     0x1
-	#define ETH_MODULE_SFF_8079_LEN 256
-	#define ETH_MODULE_SFF_8472     0x2
-	#define ETH_MODULE_SFF_8472_LEN 512
-
-	#define ETHTOOL_GMODULEINFO     0x00000042
-	#define ETHTOOL_GMODULEEEPROM   0x00000043
-#endif
-struct efx_get_module_eeprom {
-	struct ethtool_eeprom ee;
-};
-
-struct efx_get_module_info {
-	struct ethtool_modinfo info;
-};
-
 /* Set the VLAN tags for PTP receive packet filtering ***********************/
 #define EFX_TS_SET_VLAN_FILTER 0xef19
 struct efx_ts_set_vlan_filter {
@@ -489,71 +217,10 @@ struct efx_ts_hw_pps {
 	__u32 enable;
 };
 
-/* Reprogram the CPLD on an AOE NIC *****************************************/
-#define EFX_UPDATE_CPLD 0xef1e
-struct efx_update_cpld {
-	__u32 update;
-};
-
-/* License key operations on AOE or EF10 NIC ********************************/
-
-/* Deprecated - only supports AOE */
-#define EFX_LICENSE_UPDATE 0xef1f
-struct efx_update_license {
-	__u32 valid_keys;
-	__u32 invalid_keys;
-	__u32 blacklisted_keys;
-};
-
-#define EFX_LICENSE_UPDATE2 0xef23
-struct efx_update_license2 {
-	__u32 valid_keys;
-	__u32 invalid_keys;
-	__u32 blacklisted_keys;
-	__u32 unverifiable_keys;
-	__u32 wrong_node_keys;
-};
-
-#define EFX_LICENSED_APP_STATE 0xef26
-struct efx_licensed_app_state {
-	__u32 app_id;
-	__u32 state;
-};
-
-/* Reset the AOE application and controller *********************************/
-#define EFX_RESET_AOE 0xef20
-struct efx_aoe_reset {
-	__u32 flags;
-};
-
-/* Get device identity ******************************************************/
-#define EFX_GET_DEVICE_IDS 0xef22
-struct efx_device_ids {
-	__u16 vendor_id, device_id;		/* PCI device ID */
-	__u16 subsys_vendor_id, subsys_device_id; /* PCI subsystem ID */
-	__u32 phy_type;				/* PHY type code */
-	__u8 port_num;				/* port number (0-based) */
-	__u8 perm_addr[6];			/* non-volatile MAC address */
-};
-
-/* Next available cmd number is 0xef28 */
 
 /* Efx private ioctl command structures *************************************/
 
 union efx_ioctl_data {
-#ifdef EFX_NOT_EXPORTED
-	struct efx_reset_ioctl reset;
-	struct efx_evq_ack_ioctl evq_ack;
-	struct efx_set_loopback_ioctl set_loopback;
-	struct efx_mdio_ioctl mdio;
-	struct efx_set_carrier_ioctl set_carrier;
-	struct efx_set_phy_power set_phy_power;
-#endif
-	struct efx_mcdi_request mcdi_request;
-	struct efx_mcdi_request2 mcdi_request2;
-	struct efx_reset_flags reset_flags;
-	struct efx_ethtool_rxnfc rxnfc;
-	struct efx_rxfh_indir rxfh_indir;
 	struct hwtstamp_config ts_init;
 #if !defined(EFX_HAVE_NET_TSTAMP)
 	struct efx_ts_read ts_read;
@@ -563,19 +230,11 @@ union efx_ioctl_data {
 	struct efx_ts_sync ts_sync;
 	struct efx_ts_set_sync_status ts_set_sync_status;
 	struct ethtool_ts_info ts_info;
-	struct efx_get_module_eeprom eeprom;
-	struct efx_get_module_info modinfo;
 	struct efx_ts_set_vlan_filter ts_vlan_filter;
 	struct efx_ts_set_uuid_filter ts_uuid_filter;
 	struct efx_ts_set_domain_filter ts_domain_filter;
 	struct efx_ts_get_pps pps_event;
 	struct efx_ts_hw_pps pps_enable;
-	struct efx_update_cpld cpld;
-	struct efx_update_license key_stats;
-	struct efx_update_license2 key_stats2;
-	struct efx_aoe_reset aoe_reset;
-	struct efx_device_ids device_ids;
-	struct efx_licensed_app_state app_state;
 };
 
 /**
