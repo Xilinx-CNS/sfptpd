b46e1d16c1a00e139c65946e66d0d98f2c0d403a
Andrew Bower <andrew.bower@amd.com>
2023-12-24 14:24:28 +0000

    cleanup: explain sync instance recovery

--- src/sfptpd_engine.c
+++ src/sfptpd_engine.c
@@ -1990,8 +1990,13 @@ static void on_sync_instance_state_changed(struct sfptpd_engine *engine,
 		interval.tv_nsec = 0;
 	}
 
-	INFO("will switch to sync instance %s in %d seconds if %s does not recover\n",
-	     new_candidate->info.name, interval.tv_sec, engine->selected->info.name);
+	if (new_candidate == engine->selected) {
+		INFO("canceled switch away from sync instance %s as its rank recovered\n",
+		     new_candidate->info.name, interval.tv_sec, engine->selected->info.name);
+	} else {
+		INFO("will switch to sync instance %s in %d seconds if %s does not recover\n",
+		     new_candidate->info.name, interval.tv_sec, engine->selected->info.name);
+	}
 }
 
 
