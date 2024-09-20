7163717cb6ee4b27ba364a32cf33bd890207aa94
Andrew Bower <andrew.bower@amd.com>
2024-09-20 15:03:09 +0100

    cleanup: release candidate

--- src/include/sfptpd_version.h
+++ src/include/sfptpd_version.h
@@ -1,12 +1,12 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
-/* (c) Copyright 2012-2023 Xilinx, Inc. */
+/* (c) Copyright 2012-2024 Xilinx, Inc. */
 
 #ifndef _SFPTPD_VERSION_H
 #define _SFPTPD_VERSION_H
 
 
 /** Version string */
-#define SFPTPD_VERSION_TEXT  "3.8.0.1000"
+#define SFPTPD_VERSION_TEXT  "3.8.0.1001"
 
 
 #endif /* _SFPTPD_VERSION_H */
