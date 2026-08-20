5c8b7f9f1d2b4c080d8ffaa0678957399d458194
Andrew Bower <andrew.bower@amd.com>
2026-08-20 14:33:03 +0100

    SWPTP-1654: really default -Wunused-result on best-effort symlink

--- src/sfptpd_logging.c
+++ src/sfptpd_logging.c
@@ -450,7 +450,8 @@ int sfptpd_log_open(struct sfptpd_config *config)
 	/* Attempt to symlink to sfptpd instance */
 	if (strcmp(SFPTPD_STATE_PATH, state_path) &&
 	    !strncmp(SFPTPD_STATE_PATH, state_path, strlen(SFPTPD_STATE_PATH))) {
-		(void) symlink(state_path, SFPTPD_STATE_PATH);
+		if (symlink(state_path, SFPTPD_STATE_PATH)) {
+		}
 	}
 
 	/* If messages are being logged to the syslog, open it */
--- src/sfptpd_main.c
+++ src/sfptpd_main.c
@@ -445,7 +445,8 @@ static int rundir_create(struct sfptpd_config *config)
 
 	/* Best effort: create an alias to this directory */
 	if (generated_path && access(SFPTPD_RUN_DIR, F_OK))
-		(void) symlink(path, SFPTPD_RUN_DIR);
+		if (symlink(path, SFPTPD_RUN_DIR)) {
+		}
 
 	return 0;
 }
